eb07fa9b9c902e89630edacde173b74b21ec2a7c
Danilo Rodrigues <11710475+Danrrodrigues@users.noreply.github.com>
2021-03-16 22:51:03 -0300

    Update Startup.cs
    Liberando swagger

--- Startup.cs
+++ Startup.cs
@@ -68,12 +68,13 @@ namespace EasyApiRest
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
+      // Para produção descomentar
+      //if (env.IsDevelopment())
+      //{
         app.UseDeveloperExceptionPage();
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EasyApiRest v1"));
-      }
+      //}
 
       app.UseHttpsRedirection();
 
